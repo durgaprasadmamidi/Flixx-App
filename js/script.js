11865a40b2cc946cbe2933240b5f0f1320947929
durgaprasadmamidi <durgaprasad.mamidi1608@gmail.com>
2023-04-09 22:41:16 -0700

    added Page Router and active link highlight

---
+++ js/script.js
@@ -0,0 +1,39 @@
+const global = {
+    currentPage: window.location.pathname,
+};
+
+function highlightActiveLink(){
+    if(global.currentPage === '/' || global.currentPage === '/index.html'|| global.currentPage === '/shows.html'){
+        let navLink = document.querySelectorAll('.nav-link');
+        navLink.forEach((a)=>{
+            if(a.getAttribute('href')===global.currentPage){
+                a.classList.add('active');
+            }
+        })
+    }
+}
+
+
+function init() {
+    switch (global.currentPage){
+        case '/':
+        case '/index.html':
+            console.log(('Home'));
+            break;
+        case '/shows.html':
+            console.log('Shows');
+            break;
+        case '/movie-details.html':
+            console.log('Movie Details');
+            break;
+        case '/tv-details.html':
+            console.log('TV Details');
+            break;
+        case '/search.html':
+            console.log('Search');
+            break;
+    }
+    highlightActiveLink();
+}
+
+document.addEventListener('DOMContentLoaded',init);
