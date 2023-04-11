f2d825a68ec597baceaaed80da2ab222b41614b6
durgaprasadmamidi <durgaprasad.mamidi1608@gmail.com>
2023-04-10 20:39:20 -0700

    added shows and spinner

--- js/script.js
+++ js/script.js
@@ -16,7 +16,6 @@ function highlightActiveLink(){
 //Fetch top movies
 async function displayPopularMovies(){
     const {results} = await fetchAPIData('movie/popular');
-    console.log(results);
     results.forEach((movie)=>{
         const div = document.createElement('div');
         div.classList.add('card');
@@ -48,15 +47,55 @@ async function displayPopularMovies(){
     })
 }
 
+//Fetch top shoes
+async function displayPopularShows(){
+    const {results} = await fetchAPIData('tv/popular');
+    results.forEach((show)=>{
+        const div = document.createElement('div');
+        div.classList.add('card');
+        div.innerHTML = `
+          <a href="tv-details.html?id=${show.id}">
+            ${
+                show.backdrop_path
+                ? `<img
+                src="https://image.tmdb.org/t/p/w500${show.backdrop_path}"
+                class="card-img-top"
+                alt="${show.name}"
+              />`
+              : `<img
+              src="images/no-image.jpg"
+              class="card-img-top"
+              alt="${show.name}"
+            />`
+            }
+          </a>
+          <div class="card-body">
+            <h5 class="card-title">${show.name}</h5>
+            <p class="card-text">
+              <small class="text-muted">Air Date: ${show.air_date}</small>
+            </p>
+          </div>
+        `;
 
+        document.querySelector('#popular-shows').appendChild(div);
+    })
+}
+
+function showSpinner(){
+    document.querySelector('.spinner').classList.add('show');
+}
+function hideSpinner(){
+    document.querySelector('.spinner').classList.remove('show');
+}
 
 //Fetch data from TMDB API
 async function fetchAPIData(endpoint){
     const API_URL = 'https://api.themoviedb.org/3/';
     const API_KEY = 'f19d94846ffb4f85ebad07578b0acf4f';
-
+    showSpinner();
     const response = await fetch(`${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`);
     const data = await response.json();
+    hideSpinner();
     return data;
 }
 
@@ -68,7 +107,7 @@ function init() {
             displayPopularMovies();
             break;
         case '/shows.html':
-            console.log('Shows');
+            displayPopularShows();
             break;
         case '/movie-details.html':
             console.log('Movie Details');
